533f2562a921a566f9b363c91c918e74c5fe2bd6
Albert <50807004+AlbertMD@users.noreply.github.com>
2020-06-04 11:43:04 +0200

    Update configfb.php

--- configfb.php
+++ configfb.php
@@ -7,7 +7,7 @@ require_once('Facebook/autoload.php');
 
 $FBObject = new \Facebook\Facebook([
 	'app_id' => '630181460866748',
-	'app_secret' => 'c8b4d2048fb4ee5bc2cec47611f76bc2',
+	'app_secret' => 'nopass',
 	'default_graph_version' => 'v2.7'
 ]);
 
